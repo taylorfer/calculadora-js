2d0d72e0a3e80a985edffd417b3b51b126a78d18
Taylor Alves Fernandes <127779245+taylorfer@users.noreply.github.com>
2025-02-19 21:10:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
+
+function appendOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function calculate() {
+    let result;
+    switch (operator) {
+        case '+':
+            result = parseFloat(previousInput) + parseFloat(currentInput);
+            break;
+        case '-':
+            result = parseFloat(previousInput) - parseFloat(currentInput);
+            break;
+        case '*':
+            result = parseFloat(previousInput) * parseFloat(currentInput);
+            break;
+        case '/':
+            result = parseFloat(previousInput) / parseFloat(currentInput);
+            break;
+        default:
+            return;
+    }
+    currentInput = result;
+    operator = '';
+    previousInput = '';
+    display.value = result;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    display.value = '';
+}
